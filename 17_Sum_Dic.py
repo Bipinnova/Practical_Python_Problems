58cb2e1d2f2124bf9af6d50deadb4a4c8f8ae409
Riddhi Vagrecha <riddhi.vagrecha@mysetu.com>
2025-06-12 00:26:35 +0530

    Added new files and updated existing ones

---
+++ 17_Sum_Dic.py
@@ -0,0 +1,12 @@
+# c.  Write a Python program to sum all the items in a dictionary. 
+
+
+
+# Sample dictionary
+my_dict = {'a': 100, 'b': 200, 'c': 300}
+
+# Sum all the values
+total = sum(my_dict.values())
+
+# Print result
+print("Sum of all values:", total)
